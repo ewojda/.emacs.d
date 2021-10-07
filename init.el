05b7a1877d1a21aca3ea312858a4f66ff6469003
Emil <ewojda@posteo.net>
2021-10-07 19:32:02 +0200

    Refactor

--- init.el
+++ init.el
@@ -1,14 +1,17 @@
 (load "~/.emacs.d/sanemacs.el" nil t)
 
 ;;Autocomplete
-(use-package company)
-(add-hook 'after-init-hook 'global-company-mode)
 
-(use-package counsel)
-(ivy-mode 1)
 (use-package yasnippet
   :config
   (yas-global-mode 1))
+(use-package company
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
+(use-package counsel
+  :config
+  (ivy-mode 1))
+
 ;;Parens
 
 (use-package smartparens
@@ -16,32 +19,45 @@
   (smartparens-global-mode t)
   (require 'smartparens-config))
 
-;;Languages
+;;-----Languages-----
+
 (use-package lsp-mode)
-(use-package lsp-ui)
-(setq lsp-ui-doc-position 'bottom)
-;;  Go
-(use-package go-mode)
-(add-hook 'go-mode-hook 'lsp-deferred)
+(use-package lsp-ui
+  :config
+  (setq lsp-ui-doc-position 'bottom))
+
+;;Go
+
+(use-package go-mode
+  :config
+  (add-hook 'go-mode-hook 'lsp-deferred))
+
 ;;Web
 
 (use-package web-mode
   :config
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
 
+;;-----End Languages-----
+
 ;;Git
+
 (use-package magit)
 
 ;;Tab width
+
 (setq default-tab-width 4)
 (setq c-basic-offset 4)
 
 ;;Tabs
+
 (tab-bar-mode 1)
 
 ;;Theme
-(use-package monokai-theme)
-(load-theme 'monokai t)
+
+(use-package monokai-theme
+  :config
+  (load-theme 'monokai t))
 
 ;;Scrolling
 (setq scroll-preserve-screen-position t)
